50cbc96a9827100f925802cef5481fe8c1511a82
ItaloCobains <italobrandao.contato@gmail.com>
2023-03-27 15:58:26 -0300

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
-import 'package:flutter/material.dart';
 import 'package:fef_mobile_clock/screens/login_screen.dart';
-import 'package:fef_mobile_clock/screens/register_screen.dart';
+import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ferrinox Clock',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginScreen(),
-        '/register': (context) => RegisterScreen(),
-      },
+      title: 'MyApp',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        fontFamily: 'Montserrat',
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+          titleLarge: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          bodyLarge: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(),
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blue,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+          ),
+        ),
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.red),
+      ),
+      home: const LoginPage(),
     );
   }
 }
