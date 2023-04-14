e4379ea8dea365e554347047e5bbbb03fb01fa8f
ItaloCobains <italobrandao.contato@gmail.com>
2023-04-14 15:01:37 -0300

    Implementando sistema de logout

--- lib/src/components/custom_bottom_navigation_bar.dart
+++ lib/src/components/custom_bottom_navigation_bar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fef_mobile_clock/src/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
   final int currentIndex;
@@ -37,15 +40,29 @@ class CustomBottomNavigationBarState extends State<CustomBottomNavigationBar>
     super.dispose();
   }
 
+  void logout(BuildContext context) async {
+    Provider.of<UserProvider>(context, listen: false).cleanUserData();
+    Navigator.pushNamedAndRemoveUntil(
+        context, '/', (Route<dynamic> route) => false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScaleTransition(
       scale: _animation,
       child: BottomNavigationBar(
         onTap: widget.onTap,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.add_box), label: 'Temp'),
+        items: [
+          const BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          const BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Config'),
+          BottomNavigationBarItem(
+            icon: GestureDetector(
+              onTap: () => logout(context),
+              child: const Icon(Icons.logout),
+            ),
+            label: 'Sair',
+          ),
         ],
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.grey,
